729ad78f03ab87963c8ff0a0d18ba77bc234e0a4
Nihith-aitha <aithanihith22@gmail.com>
2026-05-03 16:06:57 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(
+/*app.use(
   cors({
     origin: [
       "http://localhost:5173",
@@ -20,14 +20,14 @@ app.use(
     ],
     credentials: true
   })
-);
-
-/*app.use(
-  cors({
-    origin: "https://ethara-eeqqm9no9-nihith-aithas-projects.vercel.app",
-    credentials: true,
-  })
 );*/
+
+app.use(
+  cors({
+    origin: "*",
+  
+  })
+);
 
 app.use(express.json());
 
